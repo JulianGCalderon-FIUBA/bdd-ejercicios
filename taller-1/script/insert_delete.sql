183cbc3449bc3146b0914f179eca1ed339787f65
Julian Gonzalez Calderon <gonzalezcalderonjulian@gmail.com>
2023-08-31 20:49:06 -0300

    taller 1

---
+++ taller-1/script/insert_delete.sql
@@ -0,0 +1,9 @@
+INSERT INTO teams VALUES
+	('CHILE', 0, 0, 0, 0, 0, 0, 0, 0);
+	
+SELECT * FROM teams WHERE team = 'CHILE';
+	
+DELETE FROM teams WHERE
+	team = 'CHILE';
+	
+SELECT * FROM teams WHERE team = 'CHILE';
