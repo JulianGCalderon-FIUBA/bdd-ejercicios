0e240e225c7aa4fef5904437fb229f52ad4f34b3
Julian Gonzalez Calderon <gonzalezcalderonjulian@gmail.com>
2023-09-20 19:52:17 -0300

    alternative to ej25 - taller 4

--- taller-4/soluciones/ej25.sql
+++ taller-4/soluciones/ej25.sql
@@ -1,6 +1,9 @@
-SELECT TRUNC(AVG(cantidad_notas), 2) AS promedio_cantidad_notas 
+SELECT AVG(cantidad_notas) AS promedio_cantidad_notas 
 FROM (
 	SELECT padron, COUNT(notas) AS cantidad_notas 
 	FROM notas
 	GROUP BY padron
 ) AS cantidad_notas;
+
+SELECT CAST(COUNT(*) AS FLOAT) / COUNT(DISTINCT padron) AS promedio_cantidad_notas
+FROM Notas;
