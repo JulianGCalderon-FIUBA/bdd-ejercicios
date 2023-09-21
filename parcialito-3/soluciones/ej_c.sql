42fde5ee2f35a11ed6b1ec37d79e7efe65425568
Julian Gonzalez Calderon <gonzalezcalderonjulian@gmail.com>
2023-09-20 22:55:58 -0300

    update c

--- parcialito-3/soluciones/ej_c.sql
+++ parcialito-3/soluciones/ej_c.sql
@@ -1,4 +1,5 @@
-SELECT Inscripto_En.codigo, Notas.codigo, AVG(nota)
+SELECT Inscripto_En.codigo as carrera,
+	Notas.codigo as departamento,
+	AVG(nota)
 FROM Notas INNER JOIN Inscripto_En USING(padron)
 GROUP BY Inscripto_En.codigo, Notas.codigo
-
