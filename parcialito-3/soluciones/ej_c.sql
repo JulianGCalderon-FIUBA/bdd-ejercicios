65dd33911b741b0c64c47f6f8a483eda4ffa150b
Julian Gonzalez Calderon <gonzalezcalderonjulian@gmail.com>
2023-09-27 19:07:24 -0300

    update

--- parcialito-3/soluciones/ej_c.sql
+++ parcialito-3/soluciones/ej_c.sql
@@ -1,5 +1,5 @@
 SELECT Inscripto_En.codigo as carrera,
 	Notas.codigo as departamento,
-	AVG(nota)
+	AVG(nota) as promedio
 FROM Notas INNER JOIN Inscripto_En USING(padron)
 GROUP BY Inscripto_En.codigo, Notas.codigo
--- parcialito-3/soluciones/ej_d.sql
+++ parcialito-3/soluciones/ej_d.sql
@@ -1,4 +1,4 @@
-SELECT padron, apellido, AVG(nota)
+SELECT padron, apellido, AVG(nota) as promedio
 FROM Alumnos INNER JOIN Notas USING(padron)
 GROUP BY padron
 HAVING COUNT(DISTINCT (codigo, numero)) > 3
